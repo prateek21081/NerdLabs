b2fdd84187e1966607e670ca105a25d46e9d4ff2
Prateek Kumar <prateekk724@gmail.com>
2023-03-24 20:14:08 +0530

    add: mysql connector

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, render_template
+import mysql.connector
 
 app = Flask(__name__)
+db = mysql.connector.connect(
+    host = "localhost",
+    database = "nerdlabs",
+    user = "admin",
+    password = "pass"
+)
+db.autocommit = True
+cur = db.cursor()
 
 @app.route('/')
 def root():
@@ -9,20 +18,29 @@ def root():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        if valid_login(request.form['username'], request.form['password']):
-            return 'Authenticated'
-        else:
-            return 'Try again!'
-    else:
-        return render_template('login.html')
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        return f'username: {username}, password: {password}'
+    return render_template('auth/login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        pass # register user
-        return ''
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not password:
+            error = 'Password is required.'
+        if not username:
+            error = 'Username is required.'
+        if error is None:
+            cur.execute("INSERT INTO test VALUES (%s, %s)", (username, password))
+            return 'User registered successfully'
+        else:
+            return error
     else:
-        return render_template('register.html')
+        return render_template('auth/register.html')
 
 def valid_login(username, password):
     if username == 'admin' and password == 'pass':
