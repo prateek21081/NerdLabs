e2effe2b8af9310f932cfddf5d0595dfd8ffbfad
Prateek Kumar <prateekk724@gmail.com>
2023-03-24 19:25:58 +0530

    add: login structure

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return "root"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return 'Authenticated'
+        else:
+            return 'Try again!'
+    else:
+        return render_template('login.html')
+
+def valid_login(username, password):
+    if username == 'admin' and password == 'pass':
+        return True
+    return False
