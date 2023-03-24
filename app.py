b14aa4060c267d835bc17a7a52b3789579ad06d7
Prateek Kumar <prateekk724@gmail.com>
2023-03-24 19:40:58 +0530

    add: register structure

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ def login():
     else:
         return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        pass # register user
+        return ''
+    else:
+        return render_template('register.html')
+
 def valid_login(username, password):
     if username == 'admin' and password == 'pass':
         return True
