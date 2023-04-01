74f698f28659d59ec71a5bc32f67c5014fce72c2
Prateek Kumar <prateekk724@gmail.com>
2023-04-02 00:51:56 +0530

    cleanup

--- templates/script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".menu li.icon");
-const menuItems = document.querySelector(".menu");
-
-menuIcon.addEventListener("click", () => {
-  menuItems.classList.toggle("active");
-});
