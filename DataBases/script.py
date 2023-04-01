2344ee4c95681b8cf4f458351030fa57a84a6de0
Prateek Kumar <prateekk724@gmail.com>
2023-04-01 21:08:28 +0530

    cleanup

--- DataBases/script.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-print("INSERT INTO review VALUES")
-with open('/tmp/review.csv') as csvfile:
-    records = csv.reader(csvfile)
-    for rec in records:
-        print(tuple(rec), ',')
